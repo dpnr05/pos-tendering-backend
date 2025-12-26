06fd680ff27045e11eca6252bd2de0b1e1465314
dyr3 <69321440+dyr3@users.noreply.github.com>
2025-12-26 15:56:40 -0500

    ADD DB for EB deployment-37

--- src/main/java/com/lowes/pos/tendering/entity/Tender.java
+++ src/main/java/com/lowes/pos/tendering/entity/Tender.java
@@ -1,11 +1,11 @@
 package com.lowes.pos.tendering.entity;
 
 import com.lowes.pos.tendering.model.TenderType;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 
 import java.math.BigDecimal;
